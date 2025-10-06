e22d4df1e280e75490ddd4854a28404b9f8e8754
Aankhen <Aankhen@users.noreply.github.com>
2025-10-06 15:08:54 +0530

    Cache Pug templates

--- pug/extension.js
+++ pug/extension.js
@@ -7,6 +7,7 @@ import DEFAULT_PUG_OPTIONS from './defaults.js'
 const debug 	= debugUtil('Eleventy:Plugins:Pug')
 const debugDev 	= debugUtil('Dev:Eleventy:Plugins:Pug')
 
+let cache = {}
 
 const extension = {
 	outputFileExtension: 'html',
@@ -15,6 +16,7 @@ const extension = {
 	async init() {
 		debug('Plugin initialized.')
 		debugDev('%O', this)
+		cache = {}
 	},
 
 	/**
@@ -36,39 +38,39 @@ const extension = {
 		// const filename = path.resolve('.', inputPath)
 		// debug('compile resolved inputPath to filename: %s', filename)
 
-		//	@TODO: Register Pug template dependencies for caching & performance
-		//	@see	https://www.11ty.dev/docs/languages/custom/#registering-dependencies
+		// Used to record dependencies after the fact.
+		const self = this
 
 		/** @param {Object} arg - Provided by Eleventy at runtime	*/
 		return async function(arg) {
-			debugDev('rendering... received arg: %O',		arg)
-			debug(	 'about to render... inputPath: %O', 	inputPath)
+			debug(	 'about to render... inputPath: %O',	inputPath)
 
-			//	TODO: clean up the setup and updating of options
-			const renderOptions = Object.assign(
-				{},
-				{
-					basedir: arg.eleventy.directories.includes,
-					filename: inputPath,
-					filters: extension.options.filters,
-				},
-				arg
-			)
+			// If we could get the `includes` in the outer function, we
+			// could call `compile` there and enable caching on the Eleventy
+			// side. We don’t set `cache: true` in the Pug options since we
+			// want to invalidate the cache ourselves on every build (see
+			// `init` above).
+			const renderOptions = {
+				basedir: arg.eleventy.directories.includes,
+				filename: inputPath,
+				filters: extension.options.filters,
+			}
 
-			// console.log('\n\nrenderOptions: %O\n\n', renderOptions)
+			// `inputPath` is not enough as the plugin may render multiple
+			// parts of the same template, such as the permalink and the
+			// main content.
+			const key = JSON.stringify({ renderOptions, inputSource })
+			debugDev("Render key: %O", key)
+			const compiled = (cache[key] ??= pug.compile(inputSource, renderOptions))
 
-			/*
-			 *	Using `pug.render()` is the simplest path to get this plugin working.
-			 *
-			 *	However:
-			 *		- `pug.compile()` returns a function that can be cached,
-			 *			_and_ that function has a `.dependencies` property!
-			 *
-			 *			- @see: `CustomEngine.getCompileCacheKey()`
-			 *			- would this actually speed up build times for large projects?
-			 */
-			return pug.render(inputSource, renderOptions)
+			const output = compiled(arg)
+			// See above about `includes`.
+			self.addDependencies(inputPath, output.dependencies)
+			return output
 		}
+	},
+	compileOptions: {
+		cache: false,
 	}
 }
 
